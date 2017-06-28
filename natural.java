104a2f01ac1cf9c9d0c0ccda5e72a60808aeccb4
kathirselvam97 <kathirselvam63@gmail.com>
2017-06-28 10:11:34 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class natural
+{
+public static void main(String []args)
+{
+int n;
+int k;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+k=n(n+1)/2;
+System.out.println(k);
+}
+}
+
